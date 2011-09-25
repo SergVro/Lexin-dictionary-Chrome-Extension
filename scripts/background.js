2493557c05dfc512bab7f12ff1331b1bf7ad3459
SergVro <SergVro@gmail.com>
2011-09-25 16:48:28 +0200

    history duplication processing fix

--- scripts/background.js
+++ scripts/background.js
@@ -15,9 +15,12 @@ var languages = [
     { value: "swe_som", text: "Somali" },
     { value: "swe_sdh", text: "South Kurdish" },
     { value: "swe_swe", text: "Swedish" },
-    { value: "swe_tur", text: "Turkish" }];
+    { value: "swe_tur", text: "Turkish"}];
 
-function getTranslation(word, callback) {
+// ----------------------------------------------------------------------------
+// Public section
+// ----------------------------------------------------------------------------
+function getTranslation(/* String */word, /* Function */ callback) {
     var langDirection = localStorage["defaultLanguage"];
     if (!langDirection) {
         langDirection = 'swe_swe';
@@ -32,17 +35,101 @@ function getTranslation(word, callback) {
     console.log(translation);
 }
 
+function addToHistory(/* String */langDirection, /* String */word, /* String */translation) {
+    if (!langDirection) {
+        console.error('langDirection is required');
+    }
+    if (!word) {
+        console.error('word is required');
+    }
+    
+    var history = getHistory(langDirection, false);
+    if (!history) {
+        history = new Array();
+    }
+    _addTranslationToHistory(history, translation);
+    window.localStorage.setItem(storageKey + langDirection, JSON.stringify(history));
+}
 
-function sort_by(field, reverse, primer) {
+function getHistory(/* String */ langDirection, /* bool */ compress) {
+    var history = JSON.parse(window.localStorage.getItem(storageKey + langDirection));
+    if (history && compress) {
+        // remove duplicates. We do it only on history request since we don't want to do it on every translation operation
+        _removeDuplicates(history);
+        history.sort(sort_by('added', true));
+        window.localStorage.setItem(storageKey + langDirection, JSON.stringify(history));
+    }
+    return history;
+}
 
+function clearHistory(/* String */ langDirection) {
+    window.localStorage.removeItem(storageKey + langDirection);
+}
+
+// ----------------------------------------------------------------------------
+// Private section
+// ----------------------------------------------------------------------------
+function _addTranslationToHistory(/* Array */ history, /*String*/ translation) {
+    var match = null;
+    while (match = translationRegex.exec(translation)) {
+        var wordHistory = match[1];
+        var translationHistory = match[2];
+        if (wordHistory && translationHistory) {
+            wordHistory = wordHistory.replace('|', ''); // removing vertical bars from the word
+            var d = new Date();
+            var historyItem = { word: wordHistory, translation: translationHistory, added: d.getTime() };
+            history.push(historyItem);
+            console.log('Found word ' + wordHistory + ' -> ' + translationHistory);
+        }
+        else {
+            console.error('Error parsing translation');
+        }
+    }
+}
+
+function _removeDuplicates(/*Array*/ history) {
+    for (var i = history.length - 1; i >= 0; i--) {
+        for (var j = i - 1; j >= 0; j--) {
+            if (history[i].word == history[j].word) {
+                if (history[i].translation == history[j].translation) { // if we already have the same word with the same translation
+                    console.log('Duplicate translation found for word ' + history[i].word + '. Translation ' + history[i].translation + '. Indexes ' + i + ' and ' + j);
+                    history.splice(i, 1);                               // remove it
+                    break;
+                }
+                // try to combine different translations in the list
+                console.log('Combining translation for word: ' + history[i].word + '. Translation 1 ' + history[i].translation + ' and Translation 2 ' + history[j].translation);
+                var separator = '; ';
+                var iTranslations = history[i].translation.split(separator);
+                var jTranslations = history[j].translation.split(separator);
+                var allTranslations = _combineTranslations(iTranslations, jTranslations);
+                
+                history[j].translation = allTranslations.join(separator);
+                history.splice(i, 1);                               // remove it
+                console.log('Combination resutl ' + history[j].translation);
+                break;
+            }
+        }
+    }
+}
+
+function _combineTranslations(/* Array of Strings */translations1, /* Array of Strings */translations2) {
+    var result = $.merge([], translations1);
+    for (var i = 0; i < translations2.length; i++) {
+        if ($.inArray(translations2[i], result) == -1) {
+            result.push(translations2[i]);
+        }
+    }
+    return result;
+}
+
+function sort_by(/* String */ field, /* bool */ reverse, /* Func */ primer) {
     reverse = (reverse) ? -1 : 1;
-
-    return function(a, b) {
+    return function (a, b) {
 
         a = a[field];
         b = b[field];
 
-        if (typeof(primer) != 'undefined') {
+        if (typeof (primer) != 'undefined') {
             a = primer(a);
             b = primer(b);
         }
@@ -50,63 +137,11 @@ function sort_by(field, reverse, primer) {
         if (a < b) return reverse * -1;
         if (a > b) return reverse * 1;
         return 0;
-
     };
 }
-
-function addToHistory(langDirection, word, translation) {
-    if (!langDirection) {
-        console.error('langDirection is required');
-    }
-
-    if (!word) {
-        console.error('word is required');
-    }
-    
-    var history = getHistory(langDirection, false);
-
-    if (!history) {
-        history = new Array();
-    }
-    var match = null;
-    while (match = translationRegex.exec(translation)) {
-         var wordHistory = match[1];
-         var translationHistory = match[2];
-         if (wordHistory && translationHistory) {
-             wordHistory = wordHistory.replace('|', '');
-             var d = new Date();
-             var historyItem = { word: wordHistory, translation: translationHistory, added: d.getTime() };
-             history.push(historyItem);
-             console.log('Found word ' + wordHistory + ' -> ' + translationHistory);
-         }
-         else {
-             console.error('Error parsing translation');
-         }
-    }
-     window.localStorage.setItem(storageKey + langDirection, JSON.stringify(history));
-}
-
-function getHistory(langDirection, compress) {
-    var history = JSON.parse(window.localStorage.getItem(storageKey + langDirection));
-    if (history && compress) {
-        // remove duplicates
-        for (var i = history.length - 1; i >= 0; i--) {
-            for (var j = i - 1; j >= 0; j--) {
-                if (history[i].word == history[j].word && history[i].translation == history[j].translation) {
-                    history = history.slice(0, i);
-                    break;
-                }
-            }
-        }
-        history.sort(sort_by('added', true));
-        window.localStorage.setItem(storageKey + langDirection, JSON.stringify(history));
-    }
-    return history;
-}
-
-function clearHistory(langDirection) {
-    window.localStorage.removeItem(storageKey + langDirection);
-}
+// ----------------------------------------------------------------------------
+// Initialization 
+// ----------------------------------------------------------------------------
 
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     if (request.method == "getTranslation") {
