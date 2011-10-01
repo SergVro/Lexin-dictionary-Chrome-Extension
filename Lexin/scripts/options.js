ce53468bc4cf6cc2b8d41b522824c0c889d91447
SergVro <SergVro@gmail.com>
2011-10-01 17:30:50 +0200

    Options and history restyling.
    Help added.

--- Lexin/scripts/options.js
+++ Lexin/scripts/options.js
@@ -1,7 +1,7 @@
 
 // Saves options to localStorage.
 function save_options() {
-    localStorage["defaultLanguage"] = $("#language").val();
+    localStorage["defaultLanguage"] = $("input[name='langs']:checked").val();
 
     // Update status to let user know options were saved.
     $("#status").html("Options saved");
@@ -17,15 +17,21 @@ function restore_options() {
     if (!language) {
         language = 'swe_swe';
     }
-    $("#language").val(language);
+    $("input[name='langs']").val([language]);
 }
 
 function fillLanguages(callback) {
     chrome.extension.sendRequest({ method: "getLanguages" }, function (response) {
+
         $.each(response, function (i, lang) {
-            var option = $('<option></option>').attr('value', lang.value).append(lang.text);
-            $('#language').append(option);
+            var li = $('<li></li>');
+            var input = $('<input></input>').attr('type', 'radio').attr('name', 'langs').attr('value', lang.value).attr('id', lang.value);
+            var span = $('<label></label>').attr('for', lang.value).text(lang.text);
+            li.append(input);
+            li.append(span);
+            $('#languageButtons').append(li);
         });
+        $("input[name='langs']").change(save_options);
         callback();
     });
 }
