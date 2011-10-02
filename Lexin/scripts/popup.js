3eb2406c0b075ab419e3529ab5f7a2ac0f49b161
SergVro <SergVro@gmail.com>
2011-10-02 18:13:52 +0200

    mp3 audio file links converted to the HTML5 audio tag. target='_blank' added to all links in the translation.

--- Lexin/scripts/popup.js
+++ Lexin/scripts/popup.js
@@ -1,3 +1,4 @@
+var playerTemplate = '<audio><source src="MP3_FILE_URL" type="audio/mp3" /></audio>';
 
 function getTranslation() {
     var word = $('#word').val();
@@ -7,7 +8,20 @@ function getTranslation() {
     }
     chrome.extension.sendRequest({ method: "getTranslation", word: word }, function (transData) {
         $('#translation').html(transData.translation);
-        $('a', '#translation').remove();
+
+        $('a', '#translation').attr('target', '_blank');
+
+        $.each($('a', '#translation'), function (i, anchor) {
+            var url = $(anchor).attr('href');
+            if (url.match(/mp3$/)) { // if url is a MP3 file reference - cahnge link to play audio tag
+                var playerHtml = playerTemplate.replace('MP3_FILE_URL', url);
+                $(anchor).after(playerHtml);
+                $(anchor).attr('href', '#').click(function (e) {
+                    $(this).next()[0].play();
+                    e.preventDefault();
+                });
+            }
+        });
     });
 };
 
