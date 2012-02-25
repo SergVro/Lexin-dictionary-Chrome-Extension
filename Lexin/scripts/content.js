18a68a1adeab80605485ee1589c1e957f7de6fd3
SergVro <SergVro@gmail.com>
2012-02-25 22:59:04 +0100

    Fix getSelection for iframes; Added ability to translate words displayed in translation popup; Added quick tip at pop-up window;

--- Lexin/scripts/content.js
+++ Lexin/scripts/content.js
@@ -1,13 +1,21 @@
-
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-    if (request.method == "getSelection")
-        sendResponse({ data: window.getSelection().toString() });
-    else
+    if (request.method == "getSelection") {
+        var selectedText = window.getSelection().toString();
+        if (selectedText == '') {
+            // getSelection doesn't work for iframes so we have to get a previously soted selection value
+            var savedSelection = window.localStorage.getItem('savedSelection');
+            selectedText = savedSelection;
+        }
+        sendResponse({ data: selectedText });
+    } else {
         sendResponse({});
+    }
 });
 
 $(document).click(function (evt) {
-    $(".lexinTranslationContainer").remove();
+    if ($(".lexinTranslationContainer").length > 0) {
+        $(".lexinTranslationContainer").remove();
+    }
     var selection = window.getSelection().toString();
     selection = $.trim(selection);
     if (selection && evt.altKey) {
@@ -18,7 +26,7 @@ $(document).click(function (evt) {
             $('<div></div>').attr('id', 'translation').addClass('lexinTranslationContent').html(response.translation).appendTo(container);
 
             adaptLinks($('#translation'));
-            
+
             container.position({
                 of: evt,
                 my: "left bottom",
@@ -27,5 +35,13 @@ $(document).click(function (evt) {
                 collision: "flip"
             });
         });
+    }
+});
+
+$(document).mouseup(function (evt) {
+    // saving selected text since window.getSelection doesn't work if selection is in iframe
+    var selectedText = window.getSelection().toString();
+    if (selectedText != '') {
+        window.localStorage.setItem('savedSelection', selectedText);
     }
 });
--- Lexin/scripts/popup.js
+++ Lexin/scripts/popup.js
@@ -11,6 +11,16 @@ function getTranslation() {
     });
 };
 
+function fillLanguages(callback) {
+    chrome.extension.sendRequest({ method: "getLanguages" }, function (response) {
+        $.each(response, function (i, lang) {
+            var option = $('<option></option>').attr('value', lang.value).append(lang.text);
+            $('#language').append(option);
+        });
+        callback();
+    });
+}
+
 chrome.tabs.getSelected(null, function (tab) {
     chrome.tabs.sendRequest(tab.id, { method: "getSelection" }, function (response) {
         if (response.data) {
@@ -23,18 +33,8 @@ chrome.tabs.getSelected(null, function (tab) {
     });
 });
 
-function fillLanguages(callback) {
-    chrome.extension.sendRequest({ method: "getLanguages" }, function (response) {
-        $.each(response, function (i, lang) {
-            var option = $('<option></option>').attr('value', lang.value).append(lang.text);
-            $('#language').append(option);
-        });
-        callback();
-    });
-}
-
-
 $(function () {
+
     fillLanguages(function () {
         $('#language').val(localStorage["defaultLanguage"]);
     });
@@ -44,10 +44,32 @@ $(function () {
     });
 
     $('a#historyLink').click(function (e) {
-
         chrome.tabs.create({ 'url': 'html/history.html' }, function (tab) {
 
         });
         return false;
     });
+
+    $(document).click(function(evt) {
+        var selection = window.getSelection().toString();
+        if (selection !== '') {
+            $('#word').val(selection);
+            getTranslation();
+        }
+    });
+    
+    //window.localStorage.setItem('showQuickTip', 'Yes');
+    var showQuickTip = window.localStorage.getItem('showQuickTip');
+    console.log(showQuickTip);
+    if (showQuickTip != 'No') {
+        console.log('show quick tip');
+        $('.quickTipContainer').css('display', 'block');
+        $('.quickTipContainer').click(function (e) {
+            $('.quickTipContainer').fadeOut('fast', function () {
+                $('.quickTipContainer').css('display', 'none');
+            });
+            window.localStorage.setItem('showQuickTip', 'No');
+        });
+    }
+
 });
